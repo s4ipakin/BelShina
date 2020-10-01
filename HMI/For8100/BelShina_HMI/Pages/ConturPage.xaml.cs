5094d4052fb5a765110faf459f93eb51cae89bdd
s4ipakin <62203513+s4ipakin@users.noreply.github.com>
2020-10-01 11:43:19 +0300

    BeforeChange graf

--- HMI/For8100/BelShina_HMI/Pages/ConturPage.xaml.cs
+++ HMI/For8100/BelShina_HMI/Pages/ConturPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiveCharts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,22 @@ namespace BelShina_HMI.Pages
         public ConturPage()
         {
             InitializeComponent();
+            Chart_P.ZoomingSpeed = 0.7;
+            Chart_P.DisableAnimations = true;
+            Chart_P.Zoom = ZoomingOptions.Y;
+            Chart_P.AxisX[0].Separator.Step = 1;
+            Chart_P.PreviewMouseRightButtonDown += Chart_P_PreviewMouseRightButtonDown;
+            Chart_P.PreviewMouseRightButtonUp += Chart_P_PreviewMouseRightButtonUp;
+        }
+
+        private void Chart_P_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Chart_P.Zoom = ZoomingOptions.Y;
+        }
+
+        private void Chart_P_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Chart_P.Zoom = ZoomingOptions.X;
         }
     }
 }
